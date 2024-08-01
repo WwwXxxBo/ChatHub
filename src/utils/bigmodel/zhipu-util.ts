82238358e545c71686733cb5df5e9b59601a5388
WwwXxxBo <2606489372@qq.com>
2024-08-01 15:40:18 +0800

    支持智谱AI模型

--- src/utils/bigmodel/zhipu-util.ts
+++ src/utils/bigmodel/zhipu-util.ts
@@ -1,5 +1,9 @@
+// 引入基础类型检查
+import { type ChatMessage } from "@/types"
 // 引入大模型相关类型检查
 import { type CommonChatOption } from "@/utils/bigmodel";
+// 引入接口
+import { turnChat, limitContext } from "@/utils/base-util"
 // 引入 OpenAI 库
 import OpenAI from "openai";
 import { type ChatCompletionMessageParam } from "openai/resources/chat/completions";
@@ -37,4 +41,54 @@ export const chat2zhipu = async (option: CommonChatOption) => {
     inputMaxTokens,
     contextSize
   )) as ChatCompletionMessageParam[];
+  // 流式对话
+  const stream = await openai.chat.completions.create(
+    {
+      messages: chatMessages,
+      model,
+      stream: true,
+      max_tokens: maxTokens,
+    },
+    {
+      signal: abortCtr?.signal,
+    }
+  );
+  // 开始回答
+  startAnswer && startAnswer(sessionId)
+  // 连续回答
+  for await (const chunk of stream) {
+    appendAnswer && appendAnswer(sessionId, chunk.choices[0].delta.content ?? '')
+  }
+  // 结束
+  end && end(sessionId)
 };
+
+export const getZhipuMessages = async (
+  chatMessageList: ChatMessage[],
+  instruction: string,
+  inputMaxTokens: number | undefined,
+  contextSize: number
+) => {
+  // 消息格式转换
+  let messages = await turnChat(chatMessageList)
+  // 截取制定长度的上下文
+  messages = limitContext(inputMaxTokens, contextSize, messages)
+  // 增加指令
+  if(instruction.trim().length > 0){
+    messages.unshift({
+      role: 'system',
+      content: instruction
+    })
+  }
+  // 转换消息结构
+  const openaiMessages: ChatCompletionMessageParam[] = []
+  for(const m of messages){
+    // 待添加图片处理
+    // 文本信息处理
+    openaiMessages.push({
+      role: m.role,
+      content:m.content
+    } as ChatCompletionMessageParam)
+  }
+  return openaiMessages;
+}

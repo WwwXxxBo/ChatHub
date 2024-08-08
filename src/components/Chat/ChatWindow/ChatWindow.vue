8bd78ff7071fbf2cc74670ef70670f15c0c3454e
WwwXxxBo <2606489372@qq.com>
2024-08-08 19:03:39 +0800

    支持文件上传

--- src/components/Chat/ChatWindow/ChatWindow.vue
+++ src/components/Chat/ChatWindow/ChatWindow.vue
@@ -8,7 +8,7 @@ import { type Assistant, type ChatMessage, type ChatRole } from "@/types"
 import ChatWindowHeader from "@/components/Chat/ChatWindow/ChatWindowHeader.vue";
 import ChatWindowWelcome from "@/components/Chat/ChatWindow/ChatWindowWelcome.vue";
 import ChatMultipleChoice from "@/components/Chat/ChatWindow/ChatMultipleChoice.vue"
-import ChatFileList from '@/coponents/Chat/ChatWindow/ChatFileList.vue'
+import ChatFileList from '@/components/Chat/ChatWindow/ChatFileList.vue'
 import ProviderAvatar from "@/components/Avatar/ProviderAvatar.vue";
 import UserAvatar from "@/components/Avatar/UserAvatar.vue";
 // 引入提示词列表

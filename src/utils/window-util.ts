b3dfeb87e1c7b8db65212242de7b061e66ff636a
WwwXxxBo <2606489372@qq.com>
2024-08-08 17:44:23 +0800

    获取用户选中文本

--- src/utils/window-util.ts
+++ src/utils/window-util.ts
@@ -1,12 +1,18 @@
 // 判断浏览器首选语言是否为中文
 export const isZH = (): boolean => {
-    return !!navigator.languages.at(0)?.startsWith('zh')
-}
+  return !!navigator.languages.at(0)?.startsWith("zh");
+};
 
 // 打开指定网页
 export const openInBrowser = (url: string | undefined) => {
-    if (!url) {
-      return
-    }
-    window.open(url)
+  if (!url) {
+    return;
   }
+  window.open(url);
+};
+
+// 获取选中文本
+export const getSelectedText = (defaultText: string) => {
+  const text = window.getSelection()?.toString()
+  return text == undefined || text == '' ? defaultText: text
+}

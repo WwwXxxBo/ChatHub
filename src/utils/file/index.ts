05ce0466f4ee2e5da6ea9199bfbd2a09e8f7f879
WwwXxxBo <2606489372@qq.com>
2024-08-12 23:54:53 +0800

    加入文件读取功能

--- src/utils/file/index.ts
+++ src/utils/file/index.ts
@@ -1,2 +1,26 @@
 // 与文件相关的操作
-import fs from 'fs'
+import fs from "fs";
+import { join } from "path";
+
+const appConfig = {
+  tempPath: "@/data",
+};
+
+// 临时缓存目录
+const creatTempPath = () => {
+  try {
+    fs.mkdirSync(appConfig.tempPath);
+  } catch (e: any) {
+    if (e.code != "EEXIST") {
+      console.log(e);
+    }
+  }
+};
+
+// 通过目录保存文件
+export const saveFileByPath = (path: string, fileName: string) => {
+  creatTempPath();
+  const filePath = join(appConfig.tempPath, fileName);
+  fs.copyFileSync(path, filePath)
+  return filePath
+};

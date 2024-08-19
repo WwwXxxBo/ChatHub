af17021e071bb28165528cba3e9783fafd8a9938
WwwXxxBo <2606489372@qq.com>
2024-08-19 10:16:17 +0800

    加入文件大小标准化处理方法

--- src/utils/file/index.ts
+++ src/utils/file/index.ts
@@ -21,6 +21,14 @@ const creatTempPath = () => {
 export const saveFileByPath = (path: string, fileName: string) => {
   creatTempPath();
   const filePath = join(appConfig.tempPath, fileName);
-  fs.copyFileSync(path, filePath)
-  return filePath
+  fs.copyFileSync(path, filePath);
+  return filePath;
 };
+
+// 规范输出文件大小
+export const formatFileSize = (size: number): string => {
+  if(!size || size === 0) return '0 Bytes'
+  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const digitGroups = Math.floor(Math.log(size) / Math.log(1024))
+  return parseFloat((size / Math.pow(1024, digitGroups)).toFixed(2)) + ' ' + units[digitGroups]
+}; 

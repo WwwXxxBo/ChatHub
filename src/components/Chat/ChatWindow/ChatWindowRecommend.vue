138574719381c848a9a820060c347a0b64840568
WwwXxxBo <2606489372@qq.com>
2024-08-21 16:45:45 +0800

    更新样式

--- src/components/Chat/ChatWindow/ChatWindowRecommend.vue
+++ src/components/Chat/ChatWindow/ChatWindowRecommend.vue
@@ -1,48 +1,63 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import recommends from "@/assets/json/recommends.json";
-import { useI18n } from 'vue-i18n'
+import { useI18n } from "vue-i18n";
 
-const { t } = useI18n()
+const { t } = useI18n();
 
 // 初始值为 Prompt 列表的前 4 项
-const promptList = ref([['',''],['',''],['',''],['','']]) 
+const promptList = ref([
+  ["", ""],
+  ["", ""],
+  ["", ""],
+  ["", ""],
+]);
 
 const changeRecommendation = () => {
-    let length = recommends.cn.length
-    for(const p of promptList.value){
-      let index = Math.floor(Math.random() * length)
-      p[0] = recommends.cn[index][0]
-      p[1] = recommends.cn[index][1]
-    }
-}
+  let length = recommends.cn.length;
+  for (const p of promptList.value) {
+    let index = Math.floor(Math.random() * length);
+    p[0] = recommends.cn[index][0];
+    p[1] = recommends.cn[index][1];
+  }
+};
 
-const emits = defineEmits(['selectRecommend'])
+const emits = defineEmits(["selectRecommend"]);
 const selectRecommend = (recommend: string) => {
-  emits('selectRecommend', recommend)
-}
+  emits("selectRecommend", recommend);
+};
 
 onMounted(() => {
-    // 初始化推荐列表
-    let length = recommends.cn.length
-    for(const p of promptList.value){
-      let index = Math.floor(Math.random() * length)
-      p[0] = recommends.cn[index][0]
-      p[1] = recommends.cn[index][1]
-    }
-})
+  // 初始化推荐列表
+  let length = recommends.cn.length;
+  for (const p of promptList.value) {
+    let index = Math.floor(Math.random() * length);
+    p[0] = recommends.cn[index][0];
+    p[1] = recommends.cn[index][1];
+  }
+});
 </script>
 
 <template>
   <div class="chat-window-welcome-select">
-    <span class="title">{{ $t('recommend.title') }}</span>
-    <span class="description">{{ $t('recommend.description') }}
-        <span class="changeRecommendation" @click="changeRecommendation">{{ $t('recommend.click') }}<icon-refresh /></span>
+    <span class="title">{{ $t("recommend.title") }}</span>
+    <span class="description"
+      >{{ $t("recommend.description") }}
+      <span class="changeRecommendation" @click="changeRecommendation"
+        >{{ $t("recommend.click") }}<icon-refresh
+      /></span>
     </span>
     <a-space direction="horizontal" fill>
-      <a-card v-for="(prompt, index) in promptList" :key="prompt.id" class="card" @click="selectRecommend(prompt[1])" :bordered="false" hoverable>
+      <a-card
+        v-for="(prompt, index) in promptList"
+        :key="prompt.id"
+        class="card"
+        @click="selectRecommend(prompt[1])"
+        :bordered="false"
+        hoverable
+      >
         <icon-star-fill class="card-icon" />
-        <br>
+        <br />
         <span class="card-title">{{ prompt[0] }}</span>
         <a-divider orientation="left"></a-divider>
         <span class="card-content">{{ prompt[1] }}</span>
@@ -60,40 +75,40 @@ onMounted(() => {
   align-items: center;
   justify-content: center;
   gap: 20px;
-  .title{
+  .title {
     font-size: var(--font-size-xxxl);
     font-weight: 900;
   }
-  .description{
+  .description {
     font-size: var(--font-size-lg);
     font-weight: 500;
   }
-  .changeRecommendation{
+  .changeRecommendation {
     color: rgb(var(--arcoblue-6));
   }
   .card {
     width: 180px;
     height: 230px;
     border-radius: 20px;
-    .card-icon{
-        color: rgb(var(--arcoblue-6));
-        font-size: 100;
-        margin-bottom: 10px;
+    .card-icon {
+      color: rgb(var(--arcoblue-6));
+      font-size: 100;
+      margin-bottom: 10px;
     }
-    .card-title{
-        color: rgb(var(--arcoblue-6));
-        font-size: var(--font-size-xl);
-        font-weight: 600;
-        margin-bottom: 25px;
+    .card-title {
+      color: rgb(var(--arcoblue-6));
+      font-size: var(--font-size-xl);
+      font-weight: 600;
+      margin-bottom: 25px;
     }
-    .card-content{
-        font-size: var(--font-size-lg);
-        font-weight: 500;
+    .card-content {
+      font-size: var(--font-size-lg);
+      font-weight: 500;
     }
   }
   .card:hover {
-  transform: translateY(-4px);
-  background-color: rgb(var(--arcoblue-1));
-}
+    transform: translateY(-4px);
+    background-color: rgb(var(--arcoblue-1));
+  }
 }
 </style>
--- src/components/Chat/ChatWindow/EmptyChatWindow.vue
+++ src/components/Chat/ChatWindow/EmptyChatWindow.vue
@@ -2,32 +2,77 @@
 
 <template>
   <div class="chat-window drag-area">
-    <div class="chat-window-empty">
-      <a-empty>
-        <template #image>
-          <icon-message />
-        </template>
-        {{ $t('common.slogan') }}
-      </a-empty>
-    </div>
+    <span class="title">{{ $t("recommend.title") }}</span>
+    <span class="description">支持多种大模型</span>
+    <!-- 大模型头像 -->
+    <a-space size="large" direction="vertical" align="center">
+      <a-space size="large">
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/moonshot.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/ollama.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/spark.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/ernie_bot.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/tongyi.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/tiangong.png" />
+        </a-avatar>
+      </a-space>
+      <a-space size="large">
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/openai.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/zhipu.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/step_fun.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/deep_seek.png" />
+        </a-avatar>
+        <a-avatar :size="50" shape="square" class="assistant-avatar">
+          <img alt="avatar" src="@/assets/images/baichuan.png" />
+        </a-avatar>
+      </a-space>
+    </a-space>
   </div>
 </template>
 
 <style lang="less" scoped>
 .chat-window {
-  width: 100%;
   height: 100%;
-  overflow: hidden;
+  border-radius: var(--border-radius-small);
   display: flex;
   flex-direction: column;
-  background-color: var(--color-fill-1);
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  background-color:var(--color-fill-1);
+  .title {
+    font-size: var(--font-size-xxxl);
+    font-weight: 900;
+  }
+  .description {
+    font-size: var(--font-size-lg);
+    font-weight: 500;
+  }
+  .assistant-avatar {
+    :deep(.arco-avatar-image) {
+      background-color: var(--color-bg-white);
 
-  .chat-window-empty {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+      img {
+        object-fit: contain;
+      }
+    }
   }
 }
 </style>
